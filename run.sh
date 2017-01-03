dbcafb62bb3dd0261a45d12b68fad1a39db4f0a4
Greg <reflectivecode@users.noreply.github.com>
2017-01-03 00:54:33 -0600

    better logging in script

--- run.sh
+++ run.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
-set -e
+set -euo pipefail
 SHA1=
+
+echo "[i] clean working directory"
 rm -rf *
-git clone --depth=1 "${REPO}" .
+
+echo "[i] do shallow clone of ${REPO}"
+git clone "${REPO}" . --depth=1 --quiet
+
 while true
 do
   if [ "${SHA1}" != "$(git ls-remote --exit-code origin master | awk '{print $1;}')" ]; then
-    git fetch --depth=1
-    git reset --hard origin/master
-    git clean -xdf
+    echo "[i] update detected, fetching"
+    git fetch --depth=1 --quiet
+    echo "[i] reset hard"
+    git reset origin/master --hard --quiet
+    echo "[i] clean"
+    git clean -xdf --quiet
+    echo "[i] docker-compose pull"
     docker-compose pull
+    echo "[i] docker-compose build"
     docker-compose build --force-rm
+    echo "[i] docker-compose up"
     docker-compose up -d --remove-orphans
+    echo "[i] done"
     SHA1="$(git rev-parse HEAD)"
   fi
-  if [ "${INTERVAL}" = "-1" ]; then
-    exit 0
-  fi
+  if [ "${INTERVAL}" = "-1" ]; then  exit 0; fi
   sleep ${INTERVAL}
 done
